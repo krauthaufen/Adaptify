ec1a1cf7c67cae61f7662d62ad016ec48bfb7c4d
Georg Haaser <krauthaufen@awx.at>
2019-11-07 21:13:43 +0100

    prettified Example

--- src/Examples/NetCore/Program.fs
+++ src/Examples/NetCore/Program.fs
@@ -6,37 +6,25 @@ open FSharp.Data.Adaptive
 let main _argv =
     printfn "hi there"
 
-    
-
     let ff : Adaptivefff = failwith ""
+    let a : AdaptiveRecy = failwith ""
+
     match AVal.force ff with
     | Adaptivefff.AdaptiveAaa a -> ()
     | Adaptivefff.AdaptiveGgg g -> ()
     | Adaptivefff.AdaptiveYYY -> ()
 
-    let a : AdaptiveRecy = failwith ""
+    let _test =
+        a.g.y |> AList.map (fun c ->
+            match c with
+            | AdaptiveCaseA(a, b) ->
+                a |> AVal.map float
+            | AdaptiveCaseB(x) ->
+                x
+        )
+
     match AVal.force a.g.x with
-    | AdaptiveMyUnion.AdaptiveCaseA(a, b) ->
-        ()
-    | AdaptiveCaseB(x) ->
-        ()
-    //let a = a.g.a.GetValue(Unchecked.defaultof<_>)
-
-    //match a with
-    //| :? MyUnionCaseAAdaptive<Recy,RecyAdaptive,RecyAdaptive> as a -> 
-    //    a.
-
-
-    //let x = a.a
-    //let y = a.b
-    //a.a
-
-    //let a : ModelAdaptive = failwith ""
-
-    //let (_ : alist<int>) = a.nested.list
-    //let (_ : aval<int>) = a.nested.value
+    | AdaptiveMyUnion.AdaptiveCaseA(a, b) -> ()
+    | AdaptiveMyUnion.AdaptiveCaseB(x) -> ()
     
-    //let (_ : alist<ThingAdaptive>) = a.nested2.list
-    //let (_ : ThingAdaptive) = a.nested2.value
-
     0
